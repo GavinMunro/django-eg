ea100446a7613571d18d60e71494d60a8cde4f69
gavin <ob1.data@gmail.com>
2021-03-02 21:46:31 +1000

    Add REST framework person view

--- drf/views.py
+++ drf/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Vehicle, Person, Ad, Sale, Buyer, Seller
+from .serializers import PersonSerializer, VehicleSerializer, AdSerializer, SaleSerializer
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_person(request, pk):
+    try:
+        person = Person.objects.get(pk=pk)
+    except Person.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get details of a single person
+    if request.method == 'GET':
+        return Response({})
+    # delete a single person
+    elif request.method == 'DELETE':
+        return Response({})
+    # update details of a single person
+    elif request.method == 'PUT':
+        return Response({})
+
+
+@api_view(['GET', 'POST'])
+def get_post_people(request):
+    # get all people
+    if request.method == 'GET':
+        return Response({})
+    # insert a new record for a person
+    elif request.method == 'POST':
+        return Response({})
