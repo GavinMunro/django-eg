cd33ae77fc6d6eccac12bc9e96652513c61ac0c4
gavin <ob1.data@gmail.com>
2021-03-02 21:49:02 +1000

    Add routes for views on Person.

---
+++ drf/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(
+        r'^api/v1/people/(?P<pk>[0-9]+)$',
+        views.get_delete_update_person,
+        name='get_delete_update_person'
+    ),
+    url(
+        r'^api/v1/people/$',
+        views.get_post_people,
+        name='get_post_people'
+    )
+]
