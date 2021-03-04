6b6fa14de8a28aa956f1b7413c090b36813f9572
gavin <ob1.data@gmail.com>
2021-03-04 16:00:06 +1000

    Last 3 models had : instead of =

--- drf/models.py
+++ drf/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Person(models.Model):
     """ Customer details - name phone number etc. """
-    id: models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID')
+    id = models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID')
     # id = models.BigIntegerField(primary_key=True)
     email = models.CharField(max_length=80, null=False, blank=False, unique=True)
     firstname = models.CharField(max_length=32, null=False, blank=False)
@@ -31,18 +31,18 @@ class Person(models.Model):
 
 class Vehicle(models.Model):
     """ Vehicle details """
-    id: models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID')
+    id = models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID')
     # id = models.BigIntegerField(primary_key=True)
-    vin: models.CharField(max_length=64, null=False, blank=False, unique=True)
-    rego: models.CharField(max_length=6, null=False, blank=False, unique=True)
-    make: models.CharField(max_length=32, null=False, blank=False)
-    model: models.CharField(max_length=32, null=False, blank=False)
-    year: models.IntegerField(max_length=4, null=False, blank=False)
-    # If we didn't keep history and cars could only be advertised and sold once:
-    # ad_by: models.ForeignKey(Person, on_delete=models.CASCADE)
-    # sold_to: models.ForeignKey(Person, on_delete=models.CASCADE)
-    # asking_price: models.DecimalField(decimal_places=2, null=False)
-    # agreed_price: models.DecimalField(decimal_places=2, null=False)
+    vin = models.CharField(max_length=64, null=False, blank=False, unique=True)
+    rego = models.CharField(max_length=6, null=False, blank=False, unique=True)
+    make = models.CharField(max_length=32, null=False, blank=False)
+    model = models.CharField(max_length=32, null=False, blank=False)
+    year = models.IntegerField(null=False, blank=False)
+    # If we didn't keep history and cars could only be advertised and sold once =
+    # ad_by = models.ForeignKey(Person, on_delete=models.CASCADE)
+    # sold_to = models.ForeignKey(Person, on_delete=models.CASCADE)
+    # asking_price = models.DecimalField(decimal_places=2, null=False)
+    # agreed_price = models.DecimalField(decimal_places=2, null=False)
     
     def get_vin(self):
         return self.vin
@@ -58,14 +58,14 @@ class Vehicle(models.Model):
 
 
 class Ad(models.Model):
-    seller: models.ForeignKey(Person, on_delete=models.CASCADE, null=False, blank=False)
-    vehicle: models.ForeignKey(Vehicle, on_delete=models.CASCADE, null=False, blank=False)
-    asking_price: models.DecimalField(decimal_places=2, null=False)
-    placed: models.DateField
+    seller = models.ForeignKey(Person, on_delete=models.CASCADE, null=False, blank=False)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, null=False, blank=False)
+    asking_price = models.DecimalField(decimal_places=2, max_digits=6, null=False)
+    placed = models.DateField()
     
 
 class Sale(models.Model):
-    buyer: models.ForeignKey(Person, on_delete=models.CASCADE, null=False, blank=False)
-    vehicle: models.ForeignKey(Vehicle, on_delete=models.CASCADE, null=False, blank=False)
-    sale_price: models.DecimalField(decimal_places=2)
-    bought: models.DateField
+    buyer = models.ForeignKey(Person, on_delete=models.CASCADE, null=False, blank=False)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, null=False, blank=False)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=6, null=False)
+    bought = models.DateField()
