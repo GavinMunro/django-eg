66dbc5b80f4e2b46c9c1d60984e401e8f85951b3
gavin <ob1.data@gmail.com>
2021-03-04 16:10:50 +1000

    First automated test now passes.

--- drf/tests/test_models.py
+++ drf/tests/test_models.py
@@ -6,16 +6,16 @@ class VehicleTest(TestCase):
     """ Unit test for Vehicle model - creation of 2 vehicle objects """
     def setUp(self):
         Vehicle.objects.create(
-            vin='JXA5430-1ACB29653-476', rego='TGU973', make='Toyota', model='Camry', year='2008'
+            id=1, vin='JXA5430-1ACB29653-476', rego='TGU973', make='Toyota', model='Camry', year='2008'
         )
         Vehicle.objects.create(
-            vin='VX943184-JB2346-A2', rego='HVS246', make='Holden', model='Commodore', year='2004')
+            id=2, vin='VX943184-JB2346-A2', rego='HVS246', make='Holden', model='Commodore', year='2004')
     
     def test_car_store(self):
         # Fist unit test - creation of Vehicle objects
         toyota = Vehicle.objects.get(vin='JXA5430-1ACB29653-476')
         holden = Vehicle.objects.get(vin='VX943184-JB2346-A2')
         self.assertEqual(
-            toyota.get_rego(), "TGU973 is a Toyota")
+            toyota.get_make(), "TGU973 is a Toyota")
         self.assertEqual(
-            holden.get_rego(), "HVS246 is a Holden")
+            holden.get_make(), "HVS246 is a Holden")
